315ac1fbc86dee555788788277de585ccf250077
iamsondev <sondipkumarsk@gmail.com>
2026-01-21 10:14:05 +0600

    change the providers folder location

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Navbar } from "@/components/layout/navbar";
-import { ThemeProvider } from "@/components/Providers/ThemeProviders";
+import { ThemeProvider } from "@/Providers/ThemeProviders";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
