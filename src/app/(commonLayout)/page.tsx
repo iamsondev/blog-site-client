b85146dbdc7b768f5b2d5a1a974bd0f82712720b
iamsondev <sondipkumarsk@gmail.com>
2026-01-26 01:16:42 +0600

    added user.service.ts

--- src/app/(commonLayout)/page.tsx
+++ src/app/(commonLayout)/page.tsx
@@ -1,9 +1,9 @@
 import { Button } from "@/components/ui/button";
-import { authClient } from "@/lib/auth-client";
+import { userService } from "@/services/user.services";
 
 export default async function Home() {
-  const session = await authClient.getSession();
-  console.log(session);
+  const { data } = await userService.getSession();
+  console.log(data);
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <Button>click me</Button>
---
+++ src/services/user.services.ts
@@ -0,0 +1,25 @@
+import { cookies } from "next/headers";
+
+export const userService = {
+  getSession: async function () {
+    try {
+      const cookieStore = await cookies();
+      console.log(cookieStore.toString());
+      const res = await fetch("http://localhost:5000/api/auth/get-session", {
+        headers: {
+          Cookie: cookieStore.toString(),
+        },
+        cache: "no-store",
+      });
+      const session = await res.json();
+
+      if (session === null) {
+        return { data: null, error: { message: "session data missing" } };
+      }
+      return { data: session, error: null };
+    } catch (err) {
+      console.error(err);
+      return { data: null, error: { message: "Something Went Wrong" } };
+    }
+  },
+};
